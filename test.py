60fa76c91d6c0be40a331575e9f12ea3a496a9c9
GarpunGrai <110448515+GarpunGrai@users.noreply.github.com>
2022-08-02 11:26:48 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+print("Lets go!")
+x = (1,2)
+if x == 0:
+    x == 1
+    print('x был равен 0')
+    
+elif type(x) == type(5) or type(x) == type(5.5):
+    print('x допустимое значение')
+
+else:
+    print('x недопустимое значение')
+    x=1
+
+print(100/x)
+
+
